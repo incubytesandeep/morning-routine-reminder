84500c70758f4f4616ac9e33f9909f7789ced096
Sandeep Patel <sandeep@incubyte.co>
2025-09-01 11:58:31 +0530

    Remove logs and unused method

--- src/morning-routine-reminder.ts
+++ src/morning-routine-reminder.ts
@@ -1,8 +1,6 @@
 export class MorningRoutineReminder {
     public static getReminderText(timeString: Date) {
         const hour = timeString.getHours();
-        const minute = timeString.getMinutes();
-        console.log(`Hour: ${hour}, Minute: ${minute}`);
         if(hour === 6) {
             return 'Do exercise';
         } else if (hour === 7) {
