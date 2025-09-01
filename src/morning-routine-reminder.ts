f4faf958ec9ab9b0f1a43ebd3e7bfd710e9013d5
Sandeep Patel <sandeep@incubyte.co>
2025-09-01 11:53:24 +0530

    Generate time based M=morning routine reminders

--- src/morning-routine-reminder.ts
+++ src/morning-routine-reminder.ts
@@ -1,5 +1,15 @@
 export class MorningRoutineReminder {
-    public static getReminderText() {
+    public static getReminderText(timeString: Date) {
+        const hour = timeString.getHours();
+        const minute = timeString.getMinutes();
+        console.log(`Hour: ${hour}, Minute: ${minute}`);
+        if(hour === 6) {
+            return 'Do exercise';
+        } else if (hour === 7) {
+            return 'Read and study';
+        } else if (hour === 8) {
+            return 'Have breakfast';
+        }
         return 'No activity';
     }
 }
--- test/morning-routine-reminder.spec.ts
+++ test/morning-routine-reminder.spec.ts
@@ -2,8 +2,35 @@ import { beforeEach, describe, expect, it, vi } from 'vitest';
 import {MorningRoutineReminder} from "../src/morning-routine-reminder";
 
 describe('morning routine reminder should', () => {
+
     it('return No activity when time is after 09:00am', () => {
-        const reminderText = MorningRoutineReminder.getReminderText();
+        const time = new Date("2025-01-01T09:01:00");
+        const reminderText = MorningRoutineReminder.getReminderText(time);
         expect(reminderText).toBe('No activity');
+    });
+
+    it('return No activity when time is on or before 05:59am', () => {
+        const time = new Date("2025-01-01T05:59:00");
+        const reminderText = MorningRoutineReminder.getReminderText(time);
+        expect(reminderText).toBe('No activity');
+    });
+
+    it('return Do exercise when time is between 06:00am and 06:59am', () => {
+        const time = new Date("2025-01-01T06:00:00");
+        const reminderText = MorningRoutineReminder.getReminderText(time);
+        expect(reminderText).toBe('Do exercise');
+    });
+
+
+    it('return Read and study when time is between 07:00am and 07:59am', () => {
+        const time = new Date("2025-01-01T07:00:00");
+        const reminderText = MorningRoutineReminder.getReminderText(time);
+        expect(reminderText).toBe('Read and study');
+    });
+
+    it('return Have breakfast when time is between 08:00am and 08:59am', () => {
+        const time = new Date("2025-01-01T08:00:00");
+        const reminderText = MorningRoutineReminder.getReminderText(time);
+        expect(reminderText).toBe('Have breakfast');
     });
   });
